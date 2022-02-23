806480d724d7dc4f7f99e0d6520445edf7528663
maidonghu <maidonghu@users.noreply.github.com>
2022-02-23 13:47:23 -0800

    Create build_ms_action.sh

---
+++ build_ms_action.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Install the build dependencies:
+sudo apt install build-essential flex bison dwarves libssl-dev libelf-dev
+
+# change dir to workplace
+cd "${GITHUB_WORKSPACE}" || exit
+
+cd WSL2-Linux-Kernel || exit
+
+cp ../..config .config
+
+make -j $(nproc)
+
+cd ../..
+mkdir "artifact"
